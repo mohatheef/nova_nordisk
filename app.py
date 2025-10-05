d64c8fe036a242cc525e4c2487a8bbec59d28b36
Mohammed Atheef G A <atheefga18@gmail.com>
2025-10-05 15:02:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -355,6 +355,10 @@ def incoming():
 # -------------------------------
 # Run app
 # -------------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=False, use_reloader=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+
 
